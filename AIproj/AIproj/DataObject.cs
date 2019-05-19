5f4c8720de0b0f290e46cff450ccf8593c65e4da
ktu_jonrib <jonas.ribikauskas@ktu.edu>
2019-05-19 14:16:10 +0300

    Initial commit

---
+++ AIproj/AIproj/DataObject.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIproj
+{
+    class DataObject
+    {
+        public string category { get; private set; }
+        public string headline { get; private set; }
+        public string authors { get; private set; }
+        public string link { get; private set; }
+        public string short_description { get; private set; }
+        public string date { get; private set; }
+        public DataObject(string category, string headline, string authors, string link, string short_description, string date)
+        {
+            this.category = category;
+            this.headline = headline;
+            this.authors = authors;
+            this.link = link;
+            this.short_description = short_description;
+            this.date = date;
+        }
+    }
+}
---
+++ AIproj/AIproj/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace AIproj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<DataObject> all = ReadAllData("News_Category_Dataset_v2.json");
+            Console.ReadKey();
+        }
+
+        static List<DataObject> ReadAllData(string data)
+        {
+            List<DataObject> all = new List<DataObject>();
+            using (StreamReader reader = new StreamReader(data))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    DataObject m = JsonConvert.DeserializeObject<DataObject>(line);
+                    all.Add(m);
+                }
+            }
+            return all;
+        } 
+    }
+}
